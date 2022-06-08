519541ae11b7df0b7a0302493372156f55842a27
daveckman <eckman@tamu.edu>
2022-06-08 09:42:07 -0500

    test working example of solve function
    Test working example of solve function and create .gitignore file.

--- Python/working.py
+++ Python/working.py
@@ -22,42 +22,35 @@ from utils import testsolve
 
 
 
-"""
-
-Here is a minimum reproduceable example of testsolve:
+# Here is a minimum reproduceable example of testsolve:
     
     
-my_tester = MORS_Tester(MyProblem, [0,1,2,3,4,5])
+# my_tester = MORS_Tester(MyProblem, [0,1,2,3,4,5])
 
-outs = testsolve(my_tester, MORS_solver, 20, 500, "iSCORE", delta=50, macroreps = 2, proc=1, simpar = 1)
+# outs = testsolve(my_tester, MORS_solver, 20, 500, "iSCORE", delta=50, macroreps = 2, proc=1, simpar = 1)
 
 
-Here is a minimum reproduceable example of solve:
+# Here is a minimum reproduceable example of solve:
 
     
 moop = solve(MyProblem, MORS_Tester, 20, 500, "iSCORE")
 
-MyProblem here is an example oracle, like in pymoso.
+# MyProblem here is an example oracle, like in pymoso.
 
 
-Here is a minimum reproduceable example of allocate:
+# # Here is a minimum reproduceable example of allocate:
     
-ex_prob = rand_problem_fixed(10, 3, 5)
+# ex_prob = rand_problem_fixed(10, 3, 5)
 
-boop = allocate("iSCORE", ex_prob)
+# boop = allocate("iSCORE", ex_prob)
 
-if you want to run a random problem sequentially, you use:
+# # if you want to run a random problem sequentially, you use:
     
     
-seed =  (42, 42, 42, 42, 42, 42)
-my_rng = MRG32k3a(x= seed)
+# seed =  (42, 42, 42, 42, 42, 42)
+# my_rng = MRG32k3a(x= seed)
 
-ex_prob, my_prob = allocation_to_sequential(ex_prob, my_rng)
-
-
-shoop = solve2(my_prob, MORS_Tester, 20, 500, "iSCORE")
+# ex_prob, my_prob = allocation_to_sequential(ex_prob, my_rng)
 
 
-
-
-"""
+# shoop = solve2(my_prob, MORS_Tester, 20, 500, "iSCORE")
